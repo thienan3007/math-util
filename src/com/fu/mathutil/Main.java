630aff9eb44c3100f757ce6a4dcd32165b047ef1
thienan3007 <antruong3007@gmail.com>
2021-07-15 22:23:10 +0700

    lan thu 2 lam chuyen ay

--- src/com/fu/mathutil/Main.java
+++ src/com/fu/mathutil/Main.java
@@ -13,5 +13,8 @@ public class Main {
     public static void main(String[] args) {
         long result = MathUtil.getFactorial(5);
         System.out.println("5! = 120; actual: " + result);
+        
+        // thêm code sau lần đầu tiên up code lên sv 
+        System.out.println("3! = 6; actual: " + MathUtil.getFactorial(3));
     }
 }
